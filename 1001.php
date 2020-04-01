063f86a3cf95cd768fea969e3e2620fe1110808d
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-04-01 18:52:14 +0300

    Create 1001.php

---
+++ 1001.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php if ($a==5):?>
+A is equal to 5 
+<?php endif;?>
+<?php if ($a==5){?>
+A is equal to 5 
+<?php };?>
+</body>
+</html>
